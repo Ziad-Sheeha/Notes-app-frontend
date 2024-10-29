53e53c96e24fe89dcb941ff97d6c23ab7979a1f7
zeyad sheeha <sheehaph@gmail.com>
2024-10-29 18:02:56 +0300

    change back-url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://notes-app-backend-production-feb7.up.railway.app";
+export const BASE_URL =
+      "https://hoist-hoist.onrender.com/notes-app-backend-production-feb7.up.railway.app";
