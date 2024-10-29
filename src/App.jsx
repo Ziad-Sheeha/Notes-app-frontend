a6aefd52043131cfde17043762f1cda780005360
zeyad sheeha <sheehaph@gmail.com>
2024-10-29 18:09:14 +0300

    handle entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./pages/Home/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import SignUp from "./pages/SignUp/SignUp";
 import Login from "./pages/Login/Login";
 
 const routes = (
       <Router>
             <Routes>
+                  <Route path="/" element={<Navigate to="/login" />} />
                   <Route path="/dashboard" exact element={<Home />} />
                   <Route path="/login" element={<Login />} />
                   <Route path="/signup" element={<SignUp />} />
